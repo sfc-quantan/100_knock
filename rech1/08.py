6d7664b82231dab7b48ae0ab0e577ae4c5834f88
quantan <quantan@ht.sfc.keio.ac.jp>
2017-08-21 11:06:34 +0900

    re09.py

--- rech1/08.py
+++ rech1/08.py
@@ -1,7 +1,32 @@
 # -*- coding:utf-8 -*-
+def cipher(text):
+    result=""
+    for i in text:
+        if i.islower() == True:
+            i = chr(219- ord(i))
+            result+=i
+        else:
+            result+=i
+    return result
+
+def cipher2(text):
+    result=""
+    for char in text:
+        result+= chr(219-ord(char)) if char.islower() == True else char
+    return result
+
 
 def main():
-    pass
+    s="The New York Times: Find breaking news, multimedia, reviews & opinion on Washington"
+    s=cipher(s)
+    print(s)
+    s=cipher(s)
+    print(s)
+    s=cipher2(s)
+    print(s)
+    print(cipher2(s))
+
+    
 
 if __name__ == "__main__":
     main()
--- rech1/09.py
+++ rech1/09.py
@@ -1,7 +1,38 @@
 # -*- coding:utf-8 -*-
+import random
+def change(text):
+    words = text.split(" ")
+    result=[]
+    for word in words:
+        if len(word)<4:
+            result.append(word)
+        else:
+            temp=list(word[1:-1])
+            random.shuffle(temp)
+            result.append(word[0] + "".join(temp) + word[-1])
+
+    return " ".join(result)
+
 
 def main():
-    pass
+    sentence="I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+    words=sentence.split()
+    result=""
+    for word in words:
+        if len(word) > 4:
+            temp=list(word[1:-1])
+            random.shuffle(temp)
+            result+=word[0] + "".join(temp) + word[-1]+" "
+        else:
+            result+=word+" "
+    
+    print(result)
+
+    print(change(sentence))
+
+
+
+
 
 if __name__ == "__main__":
     main()
